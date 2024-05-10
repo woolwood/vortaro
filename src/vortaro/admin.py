d5bdfb172405666172c5463fa72ecccc65fc25a1
Memduh Gokirmak <memduhg@gmail.com>
2024-05-11 00:42:10 +0200

    use autogenerated models

--- src/vortaro/admin.py
+++ src/vortaro/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from vortaro.models import Madde, Anlam, Ozellik, Anlam_ozellik, Ornek, Yazar, Atasozu, Madde_atasozu
+from vortaro.models import Madde, Anlam, Ozellik, AnlamOzellik, Ornek, Yazar, Atasozu, MaddeAtasozu
 
 # Register your models here.
 
@@ -8,8 +8,8 @@ from vortaro.models import Madde, Anlam, Ozellik, Anlam_ozellik, Ornek, Yazar, A
 admin.site.register(Madde)
 admin.site.register(Anlam)
 admin.site.register(Ozellik)
-admin.site.register(Anlam_ozellik)
+admin.site.register(AnlamOzellik)
 admin.site.register(Ornek)
 admin.site.register(Yazar)
 admin.site.register(Atasozu)
-admin.site.register(Madde_atasozu)
+admin.site.register(MaddeAtasozu)
--- src/vortaro/models.py
+++ src/vortaro/models.py
@@ -1,73 +1,114 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
-
-# Create your models here.
-
-
-class Madde(models.Model):
-    anlam_gor = models.IntegerField(null=False)
-    anlam_say = models.IntegerField(null=False)
-    birlesikler = models.TextField()
-    cesit_say = models.IntegerField(null=False)
-    cesit = models.TextField(null=False)
-    cogul_mu = models.IntegerField(null=False)
-    egik_mi = models.IntegerField(null=False)
-    font = models.TextField()
-    gosterim_tarihi = models.IntegerField(null=False)
-    kac = models.IntegerField(null=False)
-    kelime_no = models.IntegerField(null=False)
-    lisan_kodu = models.IntegerField(null=False)
-    lisan = models.TextField()
-    madde_duz = models.TextField(null=False)
-    madde = models.TextField(null=False)
-    on_taki = models.TextField()
-    ozel_mi = models.IntegerField(null=False)
-    taki = models.TextField()
-    telaffuz = models.TextField()
 
 
 class Anlam(models.Model):
-    madde_id = models.ForeignKey(Madde, on_delete=models.CASCADE)
-    anlam_sira = models.IntegerField(null=False)
-    anlam = models.TextField(null=False)
-    fiil = models.IntegerField(null=False)
-    gos = models.IntegerField(null=False)
-    tipkes = models.IntegerField(null=False)
+    anlam_id = models.AutoField(primary_key=True)
+    anlam_sira = models.IntegerField()
+    anlam = models.TextField()
+    fiil = models.IntegerField()
+    gos = models.IntegerField()
+    madde = models.ForeignKey('Madde', models.DO_NOTHING)
+    tipkes = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'anlam'
 
 
-class Ozellik(models.Model):
-    ekno = models.IntegerField(null=False)
-    kisa_adi = models.TextField(null=False)
-    tam_adi = models.TextField(null=False)
-    tur = models.IntegerField(null=False)
+class AnlamOzellik(models.Model):
+    anlam = models.ForeignKey(Anlam, models.DO_NOTHING)
+    ozellik = models.ForeignKey('Ozellik', models.DO_NOTHING)
 
-
-class Anlam_ozellik(models.Model):
-    anlam_id = models.ForeignKey(Anlam, on_delete=models.CASCADE)
-    ozellik_id = models.ForeignKey(Ozellik, on_delete=models.CASCADE)
-
-
-class Yazar(models.Model):
-    ekno = models.IntegerField(null=False)
-    kisa_adi = models.TextField(null=False)
-    tam_adi = models.TextField(null=False)
-
-class Ornek(models.Model):
-    anlam_id = models.ForeignKey(Anlam, on_delete=models.CASCADE)
-    kac = models.IntegerField(null=False)
-    ornek_sira = models.IntegerField(null=False)
-    ornek = models.TextField()
-    yazar_id = models.ForeignKey(Yazar, on_delete=models.CASCADE)
-    yazar_vd = models.TextField()
+    class Meta:
+        managed = False
+        db_table = 'anlam_ozellik'
 
 
 class Atasozu(models.Model):
-    madde_id = models.ForeignKey(Madde, on_delete=models.CASCADE)
-    madde = models.TextField(null=False)
-    on_taki = models.TextField()
+    madde = models.OneToOneField('Madde', models.DO_NOTHING, primary_key=True)
+    madde_0 = models.TextField(db_column='madde')  # Field renamed because of name conflict.
+    on_taki = models.TextField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'atasozu'
 
 
-class Madde_atasozu(models.Model):
-    madde_id = models.ForeignKey(Madde, on_delete=models.CASCADE)
-    atasozu_madde_id = models.ForeignKey(Atasozu, on_delete=models.CASCADE)
+class Madde(models.Model):
+    madde_id = models.AutoField(primary_key=True)
+    anlam_gor = models.IntegerField()
+    anlam_say = models.IntegerField()
+    birlesikler = models.TextField(blank=True, null=True)
+    cesit_say = models.IntegerField()
+    cesit = models.IntegerField()
+    cogul_mu = models.IntegerField()
+    egik_mi = models.IntegerField()
+    font = models.TextField(blank=True, null=True)
+    gosterim_tarihi = models.TextField(blank=True, null=True)
+    kac = models.IntegerField()
+    kelime_no = models.IntegerField()
+    lisan_kodu = models.IntegerField()
+    lisan = models.TextField(blank=True, null=True)
+    madde_duz = models.TextField()
+    madde = models.TextField()
+    on_taki = models.TextField(blank=True, null=True)
+    ozel_mi = models.IntegerField()
+    taki = models.TextField(blank=True, null=True)
+    telaffuz = models.TextField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'madde'
 
 
+class MaddeAtasozu(models.Model):
+    madde = models.ForeignKey(Madde, models.DO_NOTHING)
+    atasozu_madde = models.ForeignKey(Atasozu, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'madde_atasozu'
+
+
+class Ornek(models.Model):
+    ornek_id = models.AutoField(primary_key=True)
+    anlam = models.ForeignKey(Anlam, models.DO_NOTHING)
+    kac = models.IntegerField()
+    ornek_sira = models.IntegerField()
+    ornek = models.TextField(blank=True, null=True)
+    yazar = models.ForeignKey('Yazar', models.DO_NOTHING, blank=True, null=True)
+    yazar_vd = models.TextField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'ornek'
+
+
+class Ozellik(models.Model):
+    ozellik_id = models.AutoField(primary_key=True)
+    ekno = models.IntegerField()
+    kisa_adi = models.TextField()
+    tam_adi = models.TextField()
+    tur = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'ozellik'
+
+
+class Yazar(models.Model):
+    yazar_id = models.AutoField(primary_key=True)
+    ekno = models.IntegerField()
+    kisa_adi = models.TextField()
+    tam_adi = models.TextField()
+
+    class Meta:
+        managed = False
+        db_table = 'yazar'
